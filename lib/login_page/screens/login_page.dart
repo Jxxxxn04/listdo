9246f37ddbc26711ae6aac50bb3d91312cdc8fae
Jxxxxn04 <justinwolff4.11@gmail.com>
2023-06-16 21:09:44 +0200

    login_page.dart fast fertig

--- lib/login_page/screens/login_page.dart
+++ lib/login_page/screens/login_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 import 'package:listdo/constants.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final Constants constants = Constants();
+
   final _formKey = GlobalKey<FormState>();
+  final _passwordTEC = TextEditingController();
+  final _emailTEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _passwordTEC.dispose();
+    _emailTEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +43,17 @@ class LoginPage extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 8.w),
           child: Column(
             children: [
-              _registerButtonWidget(),
+              _registerButtonWidget("Registrieren"),
               _loginTextWidget("Einloggen"),
               _formWidget(),
+              _forgotPasswordTextButtonWidget("Passwort vergessen?"),
+              SizedBox(height: 5.h,),
+              _loginButtonWidget("Anmelden"),
+              SizedBox(height: 8.w,),
+              _guestButtonWidget("Als Gast fortfahren"),
+              SizedBox(height: 5.h,),
+              _lineWithWord("oder"),
+              SizedBox(height: 5.h,),
             ],
           ),
         ),
@@ -37,15 +61,15 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Widget _registerButtonWidget() {
+  Widget _registerButtonWidget(String text) {
     return SizedBox(
-      height: 20.h,
+      height: 15.h,
       width: 100.w,
       child: Row(
         children: [
           const Spacer(),
           Padding(
-            padding: EdgeInsets.only(bottom: 10.w),
+            padding: EdgeInsets.only(),
             child: Align(
               alignment: Alignment.centerRight,
               child: SizedBox(
@@ -59,12 +83,14 @@ class LoginPage extends StatelessWidget {
                     onTap: () {}, // TODO : Go to RegisterPage()
                     child: Center(
                       child: Text(
-                        "Registrieren",
+                        text,
                         textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12.sp
-                        ),
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.sp
+                          ),
+                        )
                       ),
                     ),
                   ),
@@ -77,48 +103,99 @@ class LoginPage extends StatelessWidget {
     );
   }
 
+  Widget _forgotPasswordTextButtonWidget(String text) {
+    return SizedBox(
+      height: 5.h,
+      width: 100.w,
+      child: Row(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: SizedBox(
+              height: 5.h,
+              width: 45.w,
+              child: Material(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(15),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(15),
+                  onTap: () {}, // TODO : Go to ForgotPasswordPage()
+                  child: Center(
+                    child: Text(
+                        text,
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10.sp
+                          ),
+                        )
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+
   Widget _loginTextWidget(String text) {
     return SizedBox(
-      height: 10.h,
+      height: 8.h,
       width: 100.w,
       child: Text(
         text,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 24.sp
-        ),
+        style: GoogleFonts.poppins(
+          textStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 24.sp
+          ),
+        )
       ),
     );
   }
 
   Widget _formWidget() {
     return SizedBox(
-      height: 30.h,
+      height: 18.h,
       width: 100.w,
       child: Form(
         key: _formKey,
         child: Column(
           children: [
-            _emailFormInputFieldWidget(),
+            _emailFormInputFieldWidget("Email"),
+            SizedBox(height: 4.w,),
+            _passwordFormInputFieldWidget("Password"),
           ],
         ),
       ),
     );
   }
 
-  Widget _emailFormInputFieldWidget() {
+  Widget _emailFormInputFieldWidget(String hintText) {
     return TextFormField(
+      controller: _passwordTEC,
       decoration: InputDecoration(
-        prefixIcon: Icon(Icons.email),
-        hintText: "Email",
+        prefixIcon: const Icon(Icons.email_rounded),
+        hintText: hintText,
+        hintStyle: GoogleFonts.inter(
+          textStyle: TextStyle(
+              color: const Color(0xFFACACAC),
+              fontSize: 14.sp
+          ),
+        ),
         filled: true,
         fillColor: Colors.white,
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10)
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(
+          borderSide: const BorderSide(
             color: Colors.grey,
             width: 2
           )
@@ -131,6 +208,134 @@ class LoginPage extends StatelessWidget {
         }
         return null;
       },
+    );
+  }
+
+  Widget _passwordFormInputFieldWidget(String hintText) {
+    return TextFormField(
+      controller: _emailTEC,
+      decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.lock),
+          hintText: hintText,
+          hintStyle: GoogleFonts.inter(
+            textStyle: TextStyle(
+                color: const Color(0xFFACACAC),
+                fontSize: 14.sp
+            ),
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(
+                  color: Colors.grey,
+                  width: 2
+              )
+          )
+      ),
+      // The validator receives the text that the user has entered.
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _loginButtonWidget(String text) {
+    return Container(
+      height: 7.h,
+      width: 100.w,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Material(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color(0xFF6A6FDE),
+        elevation: 6,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: () {},  // TODO : Login
+          child: Center(
+            child: Text(
+              text,
+              style: GoogleFonts.inter(
+                textStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13.sp,
+                  fontWeight: FontWeight.bold
+                )
+              )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _guestButtonWidget(String text) {
+    return Container(
+      height: 7.h,
+      width: 100.w,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Material(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color(0xFF5BA9F0),
+        elevation: 6,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: () {},  // TODO : Login
+          child: Center(
+            child: Text(
+                text,
+                style: GoogleFonts.inter(
+                    textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13.sp,
+                        fontWeight: FontWeight.bold
+                    )
+                )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _lineWithWord (String word) {
+    return SizedBox(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Expanded(
+            child: Divider(
+              thickness: 1.0,
+              endIndent: 24.0,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            word,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          const Expanded(
+            child: Divider(
+              thickness: 1.0,
+              indent: 24.0,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 
